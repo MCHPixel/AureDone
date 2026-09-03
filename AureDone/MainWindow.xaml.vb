672047e4f9df2cd3e0d418642e0bb1456d920848
MCHPixel <schulzefabian03@outlook.de>
2026-09-03 23:05:28 +0200

    Adding Custom Window Controls

--- AureDone/MainWindow.xaml.vb
+++ AureDone/MainWindow.xaml.vb
@@ -1,3 +1,15 @@
 ﻿Class MainWindow
     
+    Private Sub Button_Action_Minimize(sender As Object, e As RoutedEventArgs)
+        WindowState = WindowState.Minimized
+    End Sub
+
+    Private Sub Button_Action_Maximize(sender As Object, e As RoutedEventArgs)
+        WindowState = If(WindowState = WindowState.Maximized, WindowState.Normal, WindowState.Maximized)
+    End Sub
+
+    Private Sub Button_Action_Close(sender As Object, e As RoutedEventArgs)
+        Close()
+    End Sub
+
 End Class
